c1c0262900b290bc26e935bed5dcbb992933967a
Raitoforce34 <mahgt_34@hotmail.com>
2021-11-21 17:50:54 -0600

    update

--- src/app/features/results/page/voters/voters.component.ts
+++ src/app/features/results/page/voters/voters.component.ts
@@ -3,7 +3,7 @@ import {MatSort} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
 import {FilterService} from "../../../../core/services/filter.service";
 import {ActivatedRoute} from "@angular/router";
-import {Observable, pluck, tap} from "rxjs";
+import {merge, Observable, pluck, switchMap, tap} from "rxjs";
 
 const MPLD = 'mpld';
 
@@ -34,6 +34,7 @@ export class VotersComponent implements AfterViewInit {
   isRateLimitReached = false;
   nextURL!: string;
   prevURL!: string;
+  pageIndex = 1;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -43,12 +44,31 @@ export class VotersComponent implements AfterViewInit {
   constructor(private filterService: FilterService, private route: ActivatedRoute) {
     if (this.route.snapshot.queryParams['party'] === MPLD) {
       this.$sourceData = filterService.filterVoteFavor();
-      console.log('Voto');
     } else {
       this.$sourceData = filterService.filterNotVoteFavor();
-      console.log('NoVoto');
     }
-    this.$sourceData
+    this.suscribeTable(this.$sourceData);
+
+  }
+
+  ngAfterViewInit() {
+
+    // // If the user changes the sort order, reset back to the first page.
+    // this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    //
+    const $merge = merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        switchMap(([sort, paginator]: any) => {
+          this.isLoadingResults = true;
+          const url = this.nextURL;
+          console.log(paginator);
+          return this.filterService.changePage(url);
+        }));
+    this.suscribeTable($merge);
+  }
+
+  private suscribeTable($observable: Observable<any>) {
+    $observable
       .pipe(
         pluck('data'),
         tap(
@@ -67,42 +87,6 @@ export class VotersComponent implements AfterViewInit {
         ),
       )
       .subscribe((data: any) => (this.data = data));
-
-  }
-
-  ngAfterViewInit() {
-
-    // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
-    //
-    // merge(this.sort.sortChange, this.paginator.page)
-    //     .pipe(
-    //         startWith({}),
-    //         switchMap(() => {
-    //           this.isLoadingResults = true;
-    //           return this.exampleDatabase!.getRepoIssues(
-    //               this.sort.active,
-    //               this.sort.direction,
-    //               this.paginator.pageIndex,
-    //           ).pipe(catchError(() => observableOf(null)));
-    //         }),
-    //         map(data => {
-    //           // Flip flag to show that loading has finished.
-    //           this.isLoadingResults = false;
-    //           this.isRateLimitReached = data === null;
-    //
-    //           if (data === null) {
-    //             return [];
-    //           }
-    //
-    //           // Only refresh the result length if there is new data. In case of rate
-    //           // limit errors, we do not want to reset the paginator to zero, as that
-    //           // would prevent users from re-triggering requests.
-    //           this.resultsLength = data.total_count;
-    //           return data.items;
-    //         }),
-    //     )
-    //     .subscribe(data => (this.data = data));
   }
 
 }
