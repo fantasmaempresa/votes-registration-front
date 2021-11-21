ac75cd603c2fd9e1072e614691bca5c8a49598ea
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2021-11-21 12:15:25 -0600

    :ambulance: Modifico base_url hacia el dominio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://174.138.49.219:8080',
+  base_url: 'http://back.movimientoporlademocracia.com',
   grant_type: 'password',
   client_id: '94ed003b-e552-47a2-9d71-1a144144e5d8',
   client_secret: 'v0av4I5uUFftsqf1aWM5eFqh9kjS0O3uLWz0MozI'
