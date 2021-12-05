8555722adef551d169b9dfed0637a8943deb40de
Raitoforce34 <mahgt_34@hotmail.com>
2021-12-04 20:42:21 -0600

    ✨ añado función para obtener usuario

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -5,109 +5,113 @@ import {Tokens} from "../../data/models/tokens";
 import {map, tap} from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthService {
-    private readonly JWT_TOKEN = 'JWT_TOKEN';
-    private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
 
-    constructor(public http: HttpClient) {
-    }
+  constructor(public http: HttpClient) {
+  }
 
-    tryOfLogin({username, password}: any) {
-        let url = `${environment.base_url}/requestToLogin`;
-        let body = {
-            username,
-            password
-        };
-        return this.http.post(url, body).pipe(
-            map((data: any) => data.data)
-        );
-    }
+  tryOfLogin({username, password}: any) {
+    let url = `${environment.base_url}/requestToLogin`;
+    let body = {
+      username,
+      password
+    };
+    return this.http.post(url, body).pipe(
+      map((data: any) => data.data)
+    );
+  }
 
-    login({username, password}: any) {
-        let url = `${environment.base_url}/oauth/token`;
-        let body = {
-            grant_type: environment.grant_type,
-            client_id: environment.client_id,
-            client_secret: environment.client_secret,
-            username,
-            password
-        };
-        return this.http.post(url, body);
-    }
+  login({username, password}: any) {
+    let url = `${environment.base_url}/oauth/token`;
+    let body = {
+      grant_type: environment.grant_type,
+      client_id: environment.client_id,
+      client_secret: environment.client_secret,
+      username,
+      password
+    };
+    return this.http.post(url, body);
+  }
 
-    authorizeLogin(id: number) {
-        let url = `${environment.base_url}/authorizeUser/${id}`
-        return this.http.get(url);
-    }
+  authorizeLogin(id: number) {
+    let url = `${environment.base_url}/authorizeUser/${id}`
+    return this.http.get(url);
+  }
 
-    unauthorizeLogin(id: number) {
-        let url = `${environment.base_url}/notAuthorizeUser/${id}`;
-        return this.http.get(url);
-    }
+  unauthorizeLogin(id: number) {
+    let url = `${environment.base_url}/notAuthorizeUser/${id}`;
+    return this.http.get(url);
+  }
 
-    getDataUserLogged() {
-        let url = `${environment.base_url}/getDataUserLogged`
-        return this.http.get(url).pipe(
-            map((data:any) => data.data)
-        );
-    }
+  getDataUserLogged() {
+    let url = `${environment.base_url}/getDataUserLogged`
+    return this.http.get(url).pipe(
+      map((data: any) => data.data)
+    );
+  }
 
-    logout() {
-        this.doLogoutUser();
-    }
+  logout() {
+    this.doLogoutUser();
+  }
 
-    isLoggedIn() {
-        return !!this.getJwtToken();
-    }
+  isLoggedIn() {
+    return !!this.getJwtToken();
+  }
 
-    refreshToken() {
-        let body = {
-            grant_type: 'refresh_token',
-            client_id: environment.client_id,
-            client_secret: environment.client_secret,
-            refresh_token: this.getRefreshToken()
-        }
-        return this.http.post<any>(`${environment.base_url}/oauth/token`, body)
-            .pipe(
-                tap((tokens: Tokens) =>
-                    this.storeTokens(tokens)
-                )
-            );
+  refreshToken() {
+    let body = {
+      grant_type: 'refresh_token',
+      client_id: environment.client_id,
+      client_secret: environment.client_secret,
+      refresh_token: this.getRefreshToken()
     }
+    return this.http.post<any>(`${environment.base_url}/oauth/token`, body)
+      .pipe(
+        tap((tokens: Tokens) =>
+          this.storeTokens(tokens)
+        )
+      );
+  }
 
-    private doLoginUser(username: string, tokens: Tokens) {
-        this.storeTokens(tokens);
-    }
+  getJwtToken() {
+    return localStorage.getItem(this.JWT_TOKEN);
+  }
 
-    private doLogoutUser() {
-        this.removeTokens();
-    }
+  storeUser(user: any) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
 
-    getJwtToken() {
-        return localStorage.getItem(this.JWT_TOKEN);
-    }
+  getUser() {
+    return JSON.parse(localStorage.getItem('user') ?? '');
+  }
 
-    private getRefreshToken() {
-        return localStorage.getItem(this.REFRESH_TOKEN);
-    }
+  storeTokens(tokens: Tokens) {
+    localStorage.setItem(this.JWT_TOKEN, tokens.access_token);
+    localStorage.setItem(this.REFRESH_TOKEN, tokens.refresh_token);
+  }
 
-    private storeJwtToken(jwt: string) {
-        localStorage.setItem(this.JWT_TOKEN, jwt);
-    }
+  removeTokens() {
+    localStorage.removeItem(this.JWT_TOKEN);
+    localStorage.removeItem(this.REFRESH_TOKEN);
+  }
 
-    storeUser(user: any) {
-        localStorage.setItem('user', JSON.stringify(user));
-    }
+  private doLoginUser(username: string, tokens: Tokens) {
+    this.storeTokens(tokens);
+  }
 
-    storeTokens(tokens: Tokens) {
-        localStorage.setItem(this.JWT_TOKEN, tokens.access_token);
-        localStorage.setItem(this.REFRESH_TOKEN, tokens.refresh_token);
-    }
+  private doLogoutUser() {
+    this.removeTokens();
+  }
 
-    removeTokens() {
-        localStorage.removeItem(this.JWT_TOKEN);
-        localStorage.removeItem(this.REFRESH_TOKEN);
-    }
+  private getRefreshToken() {
+    return localStorage.getItem(this.REFRESH_TOKEN);
+  }
+
+  private storeJwtToken(jwt: string) {
+    localStorage.setItem(this.JWT_TOKEN, jwt);
+  }
 }
