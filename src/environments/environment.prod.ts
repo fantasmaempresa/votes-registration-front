7b5a849dbd5d4e8ffb2385710ad0f1e4a794b5ce
raitoforce <mahgt_34@hotmail.com>
2021-12-06 12:37:34 -0600

    🩹 corrijo url del server de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'https://back.movimientoporlademocracia.com',
+  base_url: 'https://back.movimientoporlademocracia.com/api',
   grant_type: 'password',
   client_id: '94ed003b-e552-47a2-9d71-1a144144e5d8',
   client_secret: 'v0av4I5uUFftsqf1aWM5eFqh9kjS0O3uLWz0MozI',
