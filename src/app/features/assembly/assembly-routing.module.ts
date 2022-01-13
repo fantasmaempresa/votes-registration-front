31344b2561d04d4f4184f91f26bbdc69f1038783
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2022-01-12 18:34:53 -0600

    :truck: Muevo a otro componente la parte que se tenia de asamblea para agregar nuevas caracteristicas
    El componente antes utilizado para asamblea ahora es el wrapper para poner un navbar en forma de pestañas para incorporar las nuevas caracteristicas

--- src/app/features/assembly/assembly-routing.module.ts
+++ src/app/features/assembly/assembly-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AssemblyComponent} from "./page/assembly/assembly.component";
+import {SignaturesComponent} from "./page/signatures/signatures.component";
+import {ResumeComponent} from "./page/resume/resume.component";
 
 const routes: Routes = [
   {
     path: '',
     component: AssemblyComponent,
+    children: [
+      {path: 'signatures', component: SignaturesComponent},
+      {path: 'resume', component: ResumeComponent},
+      {path: '', redirectTo: 'signatures', pathMatch: 'full'}
+    ]
   }
 ];
 
--- src/app/features/assembly/assembly.module.ts
+++ src/app/features/assembly/assembly.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AssemblyRoutingModule } from './assembly-routing.module';
 import { AssemblyComponent } from './page/assembly/assembly.component';
 import {SharedModule} from "../../shared/shared.module";
+import { SignaturesComponent } from './page/signatures/signatures.component';
+import { ResumeComponent } from './page/resume/resume.component';
 
 
 @NgModule({
   declarations: [
-    AssemblyComponent
+    AssemblyComponent,
+    SignaturesComponent,
+    ResumeComponent
   ],
   imports: [
     SharedModule,
