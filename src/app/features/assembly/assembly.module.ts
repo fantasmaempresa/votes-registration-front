b144b8c7aabc8caa2c9490c2613f0b78dc7acfb6
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2022-03-08 14:43:27 -0600

    :bug: Agrego componente faltante que no permitia la ejecucion de la app

--- src/app/features/assembly/assembly.module.ts
+++ src/app/features/assembly/assembly.module.ts
@@ -7,11 +7,13 @@ import {SharedModule} from "../../shared/shared.module";
 import { SignaturesComponent } from './page/signatures/signatures.component';
 import { ResumeComponent } from './page/resume/resume.component';
 import { CreateAssemblyComponent } from './dialog/create-assembly/create-assembly.component';
+import {AssemblyHistoryComponent} from "./page/assembly-history/assembly-history.component";
 
 
 @NgModule({
   declarations: [
     AssemblyComponent,
+    AssemblyHistoryComponent,
     SignaturesComponent,
     ResumeComponent,
     CreateAssemblyComponent
