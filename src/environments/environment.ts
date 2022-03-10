bda05381d0e30697a572154e5f9b9b3cde758f21
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2022-03-10 09:29:08 -0600

    :closed_lock_with_key: Modifico credenciales del back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   base_url: 'http://back.movimientoporlademocracia.com/api',
   grant_type: 'password',
-  client_id: '95c8025c-025c-4819-83cd-accc475bcf9f',
-  client_secret: 'J0dEz51woE8XE2mXqxZUAMDo1MC17AvS6rIiR6EJ',
+  client_id: '957f8b8d-77f9-4199-ae9b-5c5b47e3debc',
+  client_secret: 'tkuruBbq1nbDaq1s7yW4weL00kfA484Io1DrvxD3',
   mix_pusher_app_key: '123456',
   mix_pusher_app_cluster: 'mt1'
 };
