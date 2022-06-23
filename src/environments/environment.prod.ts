867ba6ed53a6d6730b65deff26482853fa6a6e1c
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2022-06-22 21:30:21 -0500

    :closed_lock_with_key: Actualizo secrets por cambios en el back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   base_url: 'https://back.movimientoporlademocracia.com/api',
   grant_type: 'password',
-  client_id: '969b58d1-6cf1-4e87-ab72-242bb131322d',
-  client_secret: 'N0e91uCgbh48XTS4f6WtWtwgSfOeYtUS4YYPWU9z',
+  client_id: '969b7e8d-6fb5-49cd-9aa8-7fe01b79b0c9',
+  client_secret: 'kbFt7IlDBR3rh1LWjO5NOMP0M997I0K404gBUh5i',
   mix_pusher_app_key: '123456',
   mix_pusher_app_cluster: 'mt1'
 };
